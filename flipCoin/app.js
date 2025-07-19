ea0cefcfde2e962425d4b27f40ef0a17f59c3b1e
Akash-29E <parmaraakash45@gmail.com>
2025-07-18 23:08:52 -0400

    WIP: Added shadow animation

--- flipCoin/app.js
+++ flipCoin/app.js
@@ -1,29 +1,37 @@
 const randomBtn = document.getElementById('randomBtn');
 const coinImg = document.getElementById('coinImg');
 const coinShadow = document.getElementById('coinShdw');
-const results = document.getElementById('results')
+const results = document.getElementById('results');
 
+// JS logic to execute flip coin functionality and update text below coin accordingly. Also updated 'RANDOM' button to stay disabled until animation completes.
 function flipCoin() {
+    coinImg.classList.remove('animation');
+    coinShadow.classList.remove('aniSh');
+    void coinImg.offsetWidth;
+    void coinShadow.offsetWidth;
+    results.textContent = '';
     const result = Math.random() < 0.5 ? "Heads" : "Tails";
     console.log(result);
-    setTimeout(()=>{
-        coinImg.classList.remove('animation');
+    setTimeout(()=>{        
         randomBtn.disabled = false;
         coinImg.disabled = false;
-    },2251)
+        results.textContent = result;
+        coinImg.style.pointerEvents = 'auto';      
+    },1000);
     if (result === 'Heads') {
         coinImg.classList.add('animation');
-        results.textContent = result;
-        coinImg.src = ``;
-        coinImg.style.animationIterationCount = 2;
+        coinShadow.classList.add('aniSh');
+        coinImg.style.animationIterationCount  = 2;
+        coinShadow.style.animationIterationCount = 2;
     } else {
         coinImg.classList.add('animation');
-        results.textContent = result;
-        coinImg.src = `resources/${result.toLowerCase()}.svg`
+        coinShadow.classList.add('aniSh');
         coinImg.style.animationIterationCount = 2.5;
+        coinShadow.style.animationIterationCount = 2.5;
     }
     randomBtn.disabled = true;
     coinImg.disabled = true;
+    coinImg.style.pointerEvents = 'none';
 }
 
 randomBtn.addEventListener('click', function (e) {
